c6cde117f9fcd352e1a754425ea6b0e2651c246f
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-05-18 16:24:45 +0600

    make admin

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req,res,next){
+  const authHeader=req.headers.authorization
+  if(!authHeader){
+    return res.status(401).send({message: "UnAuthorized access"})
+  }
+  const token=authHeader.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,function(err,decoded){
+    if(err){
+      return res.status(403).send({message : 'Forbidden access'})
+    }
+    req.decoded=decoded
+    next()
+  })
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -30,6 +46,10 @@ async function run() {
       res.send(services);
     });
 
+    app.get('/user',verifyJWT,async(req,res)=>{
+      const users=await userCollection.find().toArray()
+      res.send(users)
+    })
     app.put('/user/:email',async(req,res)=>{
       const email=req.params.email
       const user=req.body
@@ -41,6 +61,17 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc, options);
       const token=jwt.sign({email :email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
       res.send({result,accessToken : token})
+
+    })
+
+    app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
+      const email=req.params.email
+      const filter={email:email}
+      const updateDoc = {
+        $set: {role:'admin'},
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result)
 
     })
 
@@ -71,7 +102,7 @@ async function run() {
       });
       res.send(services)
     });
-    app.get('/booking', async(req, res) =>{
+    app.get('/booking',verifyJWT, async(req, res) =>{
         const patient = req.query.patient;
         const query = {patient: patient};
         const bookings = await bookingCollection.find(query).toArray();
